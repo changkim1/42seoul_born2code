96ef2bd7dd13a436ea02d1c5ac7d0b9011814125
changkim <zzankor@naver.com>
2021-11-24 16:07:30 +0900

    211124

---
+++ libft_ex/libft.h
@@ -0,0 +1,14 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_atoi(const char *str);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int i);
+int ft_isprint(int c);
+int ft_strlen(char *str);
+void	*ft_memset(void *dest, int value, size_t count);
+
+
+#endif
