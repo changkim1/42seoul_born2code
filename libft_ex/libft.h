f04742b995d98f7881fea6052df8496d3eb09ae5
changkim <zzankor@naver.com>
2022-01-20 18:05:16 +0900

    t

--- libft_ex/libft.h
+++ libft_ex/libft.h
@@ -20,7 +20,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t num);
 void	*ft_memset(void *dest, int value, size_t count);
 
-char    *ft_strchr(const char *str, int c);
+char	*ft_strchr(const char *str, int c);
 size_t	ft_strlcat(char *dest, const char *src, size_t len);
 size_t	ft_strlcpy(char *dest, const char *src, size_t n);
 size_t	ft_strlen(const char *s);
