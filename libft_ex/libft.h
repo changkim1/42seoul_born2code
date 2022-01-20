914c75e105aa524e20be6236bba268095b70a9b7
changkim <zzankor@naver.com>
2022-01-20 18:04:53 +0900

    test

--- libft_ex/libft.h
+++ libft_ex/libft.h
@@ -12,10 +12,10 @@ int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int i);
-int 	ft_isprint(int c);
+int		ft_isprint(int c);
 
 void	*ft_memchr(const void *buf, int c, size_t count);
- int	ft_memcmp(const void *buf1, const void *buf2, size_t count);
+int		ft_memcmp(const void *buf1, const void *buf2, size_t count);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t num);
 void	*ft_memset(void *dest, int value, size_t count);
@@ -26,10 +26,10 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_strncmp(const char *str1, const char *str2, size_t n);
-char    *ft_strrchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
 
-int 	ft_tolower(int c);
-int 	ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *str);
