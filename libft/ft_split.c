6229f8be9fec20dcab7b3d74e5b9f9eaee717f25
changkim <zzankor@naver.com>
2022-01-24 22:06:17 +0900

    test

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: changkim <changkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 02:53:21 by changkim          #+#    #+#             */
-/*   Updated: 2022/01/24 15:24:41 by changkim         ###   ########.fr       */
+/*   Updated: 2022/01/24 21:32:17 by changkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,17 @@ size_t	index_cnt(char const *s, char c)
 	size_t	cnt;
 
 	i = 0;
-	cnt = 1;
+	cnt = 0;
 	while (s[i])
 	{
-		if (s[i] != c && (s[i + 1] == c || s[i + 1] == 0))
+		if (s[i] == c)
+			i++;
+		else
+		{
+			while (s[i] != c && s[i])
+				i++;
 			cnt++;
-		i++;
+		}
 	}
 	return (cnt);
 }
@@ -47,8 +52,10 @@ char	*strdup_len(const char *s, size_t len)
 	size_t	i;
 	char	*str;
 
+	if (!s || len == 0)
+		return (NULL);
 	i = 0;
-	str = (char *)malloc(sizeof(char) * len);
+	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (0);
 	while (i < len)
@@ -67,24 +74,24 @@ char	**ft_split(char const *s, char c)
 	size_t	index;
 	char	**str;
 
-	i = -1;
-	index = -1;
-	str = (char **)ft_calloc(index_cnt(s, c), sizeof(char *));
+	i = 0;
+	index = 0;
+	str = (char **)malloc((index_cnt(s, c) + 1) * sizeof(char *));
 	if (!str)
 		return (0);
-	while (s[++i])
+	while (i < ft_strlen(s))
 	{
+		j = 0;
 		if (s[i] != c)
 		{
-			j = 0;
 			while (s[i + j] != c && s[i + j])
 				j++;
-			str[++index] = strdup_len(&s[i], j);
+			str[index++] = strdup_len(&s[i], j);
 			if (!str)
 				return (free_all(str));
-			i = i + j - 1;
 		}
+		i = i + j + 1;
 	}
-	str[++index] = 0;
+	str[index] = 0;
 	return (str);
 }
